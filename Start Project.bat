c513d41b420d379c08b8aed22b96b2469d5ec457
AbzDev <42191357+AbzDev@users.noreply.github.com>
2019-11-27 15:00:34 +0000

    Fixed Start Project File

--- Start Project.bat
+++ Start Project.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-cd C:\Users\abzzd\Documents\GitHub\SageTeamManagerX\TeamX
+cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\csgo-e-commerce-website\TeamX\
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
