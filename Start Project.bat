e20cf5c4c12fb0d02b5e92aaeb1360dd6761b8e4
Abzzdev <42191357+AbzDev@users.noreply.github.com>
2019-11-28 07:30:20 +0000

    Added port to start server.bat

--- Start Project.bat
+++ Start Project.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /
 REM This points to the correct directory for a local github install of the project.
-cd C:\Users\Abdullah Shah\OneDrive\Documents\GitHub\csgo-e-commerce-website\TeamX\
+cd C:\Users\abzzd\Documents\GitHub\TheGameCompanyStore\TeamX
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
 pause
